39223d4dac3fac7c84fd2df97e7c36da07951bbd
nileshbvidhate <nileshbvidhate@gmail.com>
2025-07-01 17:06:15 +0530

    Initial commit: Spring Boot project setup is done

---
+++ src/main/java/com/nil/transportx/TransPortXTransportManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nil.transportx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransPortXTransportManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransPortXTransportManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nil/transportx/TransPortXTransportManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nil.transportx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransPortXTransportManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
